38c0eaaa30b97127d97cd30664b394fa029e1237
Annehelen-ltda <71146646+Annehelen-ltda@users.noreply.github.com>
2020-10-29 19:34:47 -0300

    Type S. 13 Pass

--- endereco.ts
+++ endereco.ts
@@ -1,6 +1,51 @@
+import {isEmpty} from "./loja"
 export class Endereco {
 
     constructor(public logradouro: string, public numero: number, public complemento: string,
         public bairro: string, public municipio: string, public estado: string, public cep: string) { }
+    
+    
+    public endereco_info(){
+        this.validacao_endereco()
+        let numero : string
+        if(this.numero <= 0){
+            numero = "s/n"
+        }
+        else{
+            numero = this.numero.toString()
+        }
 
+        var part2 = `${this.logradouro}, ${numero}`;
+        if (! isEmpty (this.complemento)){
+            part2 += ` ${this.complemento}`;
+        }
+        var part3 = "";
+        if (! isEmpty (this.bairro)){
+            part3 += `${this.bairro} - `;
+        }
+        part3 += `${this.municipio} - ${this.estado}`;
+        
+        var part4 = "";
+        if (! isEmpty (this.cep)){
+            part4 = `CEP:${this.cep}`;
+        }
+
+        let ende = `${part2}\n`
+        ende += `${part3}\n`
+        ende += `${part4}`
+        return ende
+
+    }
+    public validacao_endereco(){
+        if(isEmpty(this.logradouro)){
+            throw new Error(`O campo logradouro do endereço é obrigatório`);
+        }
+        if(isEmpty(this.municipio)){
+            throw new Error(`O campo município do endereço é obrigatório`);
+        }
+        if(isEmpty(this.estado)){
+            throw new Error(`O campo estado do endereço é obrigatório`);
+        }
+
+    }
 }
--- venda.ts
+++ venda.ts
@@ -1,3 +1,94 @@
+import { ItemVenda } from "./ItemVenda";
+import { isEmpty, Loja } from "./loja"
+
 export class Venda {
+    constructor(
+        public loja: Loja,
+        public datahora: Date,
+        public ccf: string,
+        public coo: string
+    ){}
+    
+    itens = new Array <ItemVenda>()
+
+    adcionar_item (
+        item: number, 
+        codigo:number, 
+        descricao: string, 
+        quantidade: number, 
+        unidade: string, 
+        valor_unitario: number, 
+        substituicao_tributaria: string) {
+            let item_venda = new ItemVenda(item, codigo, descricao, quantidade, unidade,
+                valor_unitario, substituicao_tributaria)
+            this.itens.push(item_venda)
+        
+    }
+
+    dados_vendas (){
+        this.validacao_venda()
+
+        let ano = this.datahora.getFullYear()
+        let mes = this.datahora.getMonth()
+        let dia = this.datahora.getDate()
+        let hora = this.datahora.getHours()
+        let minuto = this.datahora.getMinutes()
+        let segundo = this.datahora.getSeconds()
+
+        let texto_data = `${dia}/${mes}/${ano}`
+        let texto_hora = `${hora}:${minuto}:${segundo}`
+        
+        let dadosT = texto_data
+        dadosT += " " + texto_hora + "V"
+        dadosT += " CCF:" + this.ccf
+        dadosT += " COO: " + this.coo
+
+        return dadosT
+    }
+    
+    dados_itens (){
+        let dados = ["ITEM CODIGO DESCRICAO QTD UN VL UNIT(R$) ST VL ITEM(R$)\n"]
+        
+        this.itens.forEach((item_linha)=>{
+            let valor_item = item_linha.quantidade * item_linha.valor_unitario
+            let linha = `${item_linha.item} ${item_linha.codigo} ${item_linha.descricao} ${item_linha.quantidade} ${item_linha.unidade} ${item_linha.valor_unitario} ${item_linha.substituicao_tributaria} ${item_linha.total()}`
+            dados.push(linha)
+            return dados.join("\n")
+        })
+
+    }
+
+    calcular_total(){
+        let total = 0
+        this.itens.forEach((item_linha)=>{total += item_linha.total( )})
+        return total
+
+    }
+
+    imprimir_cupom(){
+        let dados_loja = this.loja.dados_loja()
+        let dados_vendas = this.dados_vendas()
+        let dados_itens = this.dados_itens()
+        let total = this.calcular_total()
+
+        let cupons_dados = `${dados_loja}\n`
+        cupons_dados += `${dados_vendas}\n`
+        cupons_dados += `${dados_itens}\n`
+        cupons_dados += `${total}`
+        return cupons_dados
+
+    }
+    
+    validacao_venda(){
+        this.loja.validacao_loja()
+        if(isEmpty(this.ccf)){
+            throw new Error("O campo de ccf é obrigatório")
+        }
+        if(isEmpty(this.coo)){
+            throw new Error("O campo de coo é obrigatório")
+        }
+            
+    }
+    
 
 }
